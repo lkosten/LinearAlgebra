3253bee9510404b165c62480aec5a81d92a1eade
Kostya Yarmosh <kyarmash@gmail.com>
2019-12-04 18:59:16 +0300

    transform matrix to diagonally dominant

--- LinearAlgebra/main.cpp
+++ LinearAlgebra/main.cpp
@@ -13,34 +13,37 @@ int main()
 
   input >> example;
   input >> terms;
-  input >> example >> terms;
 
-  //std::cout << example << std::endl;
+  std::cout << std::setprecision(30) << example.SimpleIterationTechique(terms) << std::endl;
 
-  /*example.elementaryTransformation(3, 0, 2);
-  example.elementaryTransformation(1, 0, 2);
+
+  example.elementaryTransformation(3, 0, 2);
   example.elementaryTransformation(1, 2, -1);
+  example.elementaryTransformation(1, 0, 2);
   example.elementaryTransformation(1, 3, 1);
 
-  example.elementaryTransformation(2, 1, 3);
-  example.elementaryTransformation(2, 3, -2);
-  example.elementaryTransformation(2, 0, -8);
+  example.elementaryTransformation(2, 2, 1.787);
+  example.elementaryTransformation(2, 0, -1.15);
+  example.elementaryTransformation(2, 1, -0.642);
+  example.elementaryTransformation(2, 3, -0.609);
+
 
   terms.elementaryTransformation(3, 0, 2);
-  terms.elementaryTransformation(1, 0, 2);
   terms.elementaryTransformation(1, 2, -1);
+  terms.elementaryTransformation(1, 0, 2);
   terms.elementaryTransformation(1, 3, 1);
 
-  terms.elementaryTransformation(2, 1, 3);
-  terms.elementaryTransformation(2, 3, -2);
-  terms.elementaryTransformation(2, 0, -8);*/
+  terms.elementaryTransformation(2, 2, 1.787);
+  terms.elementaryTransformation(2, 0, -1.15);
+  terms.elementaryTransformation(2, 1, -0.642);
+  terms.elementaryTransformation(2, 3, -0.609);
 
-  std::cout << example << std::endl << terms;
+
+  std::cout << example << std::endl << terms << std::endl;
   
   std::cout << std::setprecision(30) << example.GaussianElimination(terms) << std::endl;
   std::cout << std::setprecision(30) << example.GaussianSeidelMethod(terms) << std::endl;
   std::cout << std::setprecision(30) << example.JacobiMethod(terms) << std::endl;
-  std::cout << std::setprecision(30) << example.SimpleIterationTechique(terms) << std::endl;
 
 
 
